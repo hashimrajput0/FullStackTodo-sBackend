2f018bc3699cb5cbd6cf7b4a45b2a856f2b5ccbd
Muhammad Hashim <hashimrajput991@gmail.com>
2026-06-09 12:49:58 +0500

    Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import cors from "cors"
 const app = express() 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://fullstacktodos-hashim.netlify.app/",
     credentials: true,
 }))
 
